5679f643a10c46365f2f534e0d8c4cc1eaedc7e2
orzorzooo <orzorzooo@gmail.com>
2022-09-26 14:10:14 +0800

    EDIT: json 及 urlencode的大小上限

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb' }));
+
   await app.listen(3000);
 }
 bootstrap();
